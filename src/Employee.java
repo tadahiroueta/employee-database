079d141fcdcb710958dacb0e4e827ff2bcf22c4a
tadahiroueta <tadahiroueta@gmail.com>
2023-03-29 15:57:37 -0500

    finished

--- src/Employee.java
+++ src/Employee.java
@@ -1,21 +1,20 @@
 public class Employee {
     protected int id;
     protected String name;
-    final protected static int TABLE_SIZE = 10; // TODO change once there is more information 
 
     public Employee(String name, int id) {
         this.name = name;
         this.id = id;
     }
 
-    public int hashCode() {
-        return id;
+    public int hashCode(int tableSize) {
+        return id % tableSize;
     }
 
     public boolean equals(Object other) {
         if (other instanceof Employee) {
             Employee otherEmployee = (Employee) other;
-            return this.id == otherEmployee.id;
+            return this.id == otherEmployee.id && this.name == otherEmployee.name;
         }
         return false;
     }
--- src/EmployeeDatabase.java
+++ src/EmployeeDatabase.java
@@ -1,3 +1,72 @@
 public class EmployeeDatabase {
+    protected int tableSize = 11;
+    private Employee[] set;
+    private int size = 0;
 
+    public EmployeeDatabase() { set = new Employee[tableSize]; }
+
+    public EmployeeDatabase(int tableSize) {
+        this.tableSize = tableSize;
+        set = new Employee[tableSize];
+    }
+
+    public int size() { return size; }
+
+    public boolean add(Employee key) {
+        for (int i = key.hashCode(tableSize); i < tableSize; i++) {
+            int theRealI = i % tableSize;
+            Employee servant = set[theRealI];
+            if (servant == null) {
+                set[theRealI] = key;
+                size++;
+                return true;
+            }
+            if (servant.equals(key)) break;
+        }
+        return false;
+    }
+
+    public boolean contains(Employee key) {
+        for (int i = key.hashCode(tableSize); i < tableSize; i++) {
+            int theRealI = i % tableSize;
+
+            if (set[theRealI] == null) break;
+            if (set[theRealI].equals(key)) return true;
+        }
+        return false;
+    }
+
+    private boolean justRemove(Employee key) {
+        for (int i = key.hashCode(tableSize); i < tableSize; i++) {
+            int theRealI = i % tableSize;
+            if (set[theRealI] == null) break;
+
+            if (set[theRealI].equals(key)) {
+                set[theRealI] = null;
+                size--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean remove(Employee key) {
+        if (!justRemove(key)) return false;
+        
+        for (int i = 0; i < tableSize; i++) {
+            Employee temp = set[i];
+            if (temp == null) continue;
+
+            set[i] = null;
+            add(temp);
+        }
+        return true; 
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        for (Employee employee : set) output += (employee == null ? "null" : employee.name) + ", ";
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
 }
